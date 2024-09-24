cace5f250f425a4d6ddd4a9fdf83218ee79cca5d
JoseGabrielll <josegabrielpe@hotmail.com>
2024-09-23 23:46:26 -0300

    feat: added get and post user

--- app/backend/route/user_route.py
+++ app/backend/route/user_route.py
@@ -9,7 +9,19 @@ from app.backend.service.user_service import UserService
 user_router = APIRouter(prefix="/api/user")
 
 @user_router.post("/", response_model=User, tags=['User'], status_code=201)
-async def create_user(user: User, database: Session = Depends(get_database)):
+async def create_user(user: User, database: Session = Depends(get_database)) -> User:
+    """It creates a user
+
+    Args:
+        user (User): user schema
+        database (Session, optional): database session. Defaults to Depends(get_database).
+
+    Raises:
+        HTTPException: it returns 400 for generic exceptions
+
+    Returns:
+        User: user object from database
+    """
     try:
         return UserService.create_user(user)
     except Exception as error:
@@ -18,7 +30,20 @@ async def create_user(user: User, database: Session = Depends(get_database)):
 
 
 @user_router.get("/{id}", response_model=User, tags=['User'])
-async def get_user(id: int, database: Session = Depends(get_database)):
+async def get_user(id: int, database: Session = Depends(get_database)) -> User:
+    """It gets the user by id
+
+    Args:
+        id (int): user id
+        database (Session, optional): database session. Defaults to Depends(get_database).
+
+    Raises:
+        http_error: it returns 404 if user doesn`t exists
+        HTTPException: it returns 400 for generic exceptions
+
+    Returns:
+        User: user object from database
+    """
     try:
         return UserService.get_user_by_id(id)
     except HTTPException as http_error:
